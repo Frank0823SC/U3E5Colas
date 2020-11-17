64662233168a6526770f131d06425960e988c38e
Frank0823SC <71744464+Frank0823SC@users.noreply.github.com>
2020-11-17 17:59:36 -0600

    Update Main.java

--- Colas/Main.java
+++ Colas/Main.java
@@ -1,1 +1,41 @@
-
+package Colas;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        
+        Scanner leer = new Scanner(System.in);
+        boolean continuar = true;
+        String pal;
+        int opcion;        
+        Cola yo = new Cola();
+        
+        while(continuar){
+            System.out.println("");
+            System.out.println("*********************************************");
+            System.out.println("********************Menú*********************");
+            System.out.println("****1.- Insertar                         ****");
+            System.out.println("****2.- Eliminar                         ****");
+            System.out.println("****3.- Mostrar                          ****");            
+            System.out.println("****4.- Salir                            ****");
+            System.out.println("*********************************************");
+            System.out.println("*********************************************");            
+            opcion = leer.nextInt();
+            
+            switch(opcion){
+                case 1 -> {                    
+                    System.out.println("Ingrese palabra: ");
+                    pal = leer.next();
+                    yo.Insertar(pal);
+                }      
+                case 2 ->{                    
+                    yo.Eliminar();
+                }
+                case 3 -> {                    
+                    yo.Mostrar();
+                }
+                case 4 -> continuar = false;
+                default -> System.out.println("Opción incorrecta.....Vuelve a intentarlo");                                                  
+            }
+        }         
+    }
+}
