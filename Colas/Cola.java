858c61cd5aa52c31f44b2fe17abb2b48517ad880
Frank0823SC <71744464+Frank0823SC@users.noreply.github.com>
2020-11-17 18:00:22 -0600

    Update Cola.java

--- Colas/Cola.java
+++ Colas/Cola.java
@@ -1,1 +1,47 @@
-
+package Colas;
+public class Cola {
+        
+    NodoCola UltVal,fiin;
+    int tam;
+    
+    public boolean Vacía(){
+        return UltVal == null;
+    }
+    
+    public void Insertar(String dato){
+        NodoCola nod = new NodoCola(dato);
+        if(Vacía()){
+            UltVal = nod;
+        }
+        else{
+            fiin.sig = nod;
+        }
+        fiin = nod;
+        tam++;
+    }
+    
+    public String Eliminar(){
+        if(!Vacía()){
+            UltVal = UltVal.sig;
+            tam--;
+            System.out.println("Se elimino un elemento.....");
+        }
+        else{
+            System.out.println("La cola no tiene elementos que eliminar.....");
+        }
+        return null;
+    }
+    
+    public void Mostrar(){
+        NodoCola recorrer = UltVal;
+        if(!Vacía()){
+            while(recorrer != null){                
+                System.out.print("["+recorrer.dato+"] ");                
+                recorrer = recorrer.sig;
+            }
+        }
+        else{
+            System.out.println("La cola se encuentra vacía.....");
+        }
+    }
+}
