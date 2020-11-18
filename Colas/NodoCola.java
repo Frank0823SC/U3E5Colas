097e36a5ad19b14fc252af9ed53da078497783ec
Frank0823SC <71744464+Frank0823SC@users.noreply.github.com>
2020-11-17 18:00:51 -0600

    Update NodoCola.java

--- Colas/NodoCola.java
+++ Colas/NodoCola.java
@@ -1,1 +1,11 @@
+package Colas;
 
+public class NodoCola {
+    String dato;
+    NodoCola sig;
+    
+    public NodoCola(String dato){
+        this.dato = dato;
+        this.sig = null;
+    }
+}
